acffc78e0d30da3049812a942e10068c0ba5cd86
philip <865780943@qq.com>
2019-06-08 11:36:48 +0800

    first commit.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # fontmaker
 自定义数量字体生成
+
+## 安装
+
+>npm install --save fontmin
+
+## 运行
+
+>./build.sh
+
+## 自定义有限文字
+
+>vim build.sh
---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+FILE="diyfont.ttf"
+
+fontmin -t '制作晒坯修坯上釉烧制交付帮助与陶艺地图体验个人中心知识我的作品集报名反馈' huawenlishu.ttf > $FILE
